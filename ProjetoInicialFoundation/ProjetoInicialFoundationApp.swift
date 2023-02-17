9bcd9a80d99c9c2a3c9bdcf524e2469ec457414d
Gustavo Barreto Holzmann de Vasconcelos <gustavobarretohv@live.com>
2023-02-17 17:24:47 -0300

    Initial Commit

---
+++ ProjetoInicialFoundation/ProjetoInicialFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoInicialFoundationApp.swift
+//  ProjetoInicialFoundation
+//
+//  Created by user on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoInicialFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
